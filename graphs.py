d6c524e3d0df75e3a2ad357863639661855975a9
ajeddin <abduljeddin@gmail.com>
2023-01-12 21:10:37 +0300

    done=

--- graphs.py
+++ graphs.py
@@ -1,13 +1,13 @@
 ############################# TO DO #####################################
 
 # write import statement for psycopg2
-
+import psycopg2
 # write import statement to import Error from psycopg2
-
+from psycopg2 import Error
 # import the get_connected function from your database.py file
-
+from database import get_connected
 # import matplotlib.pyplot as plt
-
+import matplotlib.pyplot as plt
 
 try:
     # connection to database
@@ -19,17 +19,45 @@ try:
 ############################# TO DO #####################################
 
 ##### create product_revenue_query with the query from parts 3 and 4 which get the total revenue from each product category
+    product_revenue_query = """
+    SELECT product_category, SUM(unit_price*quantity)
+                            FROM invoices
+                            GROUP BY product_category
+                            ORDER BY SUM DESC
+    """
+    #### follow the project directions to create a function called get_revenue which fetches the results from the product_revenue_query
+    def get_revenue():
+        with connection:
+            with cursor:
+                cursor.execute(product_revenue_query)
+                return cursor.fetchall()
 
-#### follow the project directions to create a function called get_revenue which fetches the results from the product_revenue_query
+
 
 #### create a variable called product_revenue and set it equal to the get_revenue function invoked 
-
+    product_revenue = get_revenue()     
 #### print the product_revenue variable to look at the structure of the variable to help you write the next function
-
+    print(product_revenue)
 #### write a function which loops through the product_revenue variable and creates two lists - one with product categories and one with total revenue values
-
+    xlist = []
+    ylist = []
+    for i in range(len(product_revenue)):
+        xlist.append(product_revenue[i][0])
+        ylist.append(int(product_revenue[i][1]))
 #### follow the project directions to create a function which makes a bar chart in matplotlib using the total revenue from each product category
+    def barChart():
+        figure = plt.figure()
+        data = ylist
+        labels = xlist
 
+        plt.xticks(range(len(labels)),labels,rotation=70)
+        plt.xlabel('Product Category')
+        plt.ylabel('Revenue (USD)')
+        plt.title("Product Revenue by Category")
+        plt.bar(labels,data)
+        return figure
+    barChart()
+    plt.show()
 #### invoke the create_bar_chart function
 
 #### use plt.show() to show a pop-up of the bar chart you created
--- queries.py
+++ queries.py
@@ -16,48 +16,66 @@ try:
     ############# SQL QUERIES TO EXTRACT INFORMATION #################
 
     #### create master_query variable
+    master_query = """ SELECT * FROM invoices
+                JOIN customers
+                on invoices.customer_id = customers.customer_id
+ """
+    
     #### create outdoors_query variable
-    outdoors_query = """select * from invoices 
-    join customers 
-    on invoices.customer_id = customers.customer_id
-    where invoices.product_category = 'Outdoors';
-    """
+    outdoors_query = """ select * from invoices 
+                    join customers 
+                    on invoices.customer_id = customers.customer_id
+                    WHERE invoices.product_category = 'Outdoors' """
+
+    
 
     #### create garden_query variable
-    outdoors_query = """select * from invoices 
-    join customers 
-    on invoices.customer_id = customers.customer_id
-    where invoices.product_category = 'Outdoors';
+    garden_query = """  
+    SELECT * FROM invoices
+    JOIN customers on invoices.customer_id = customers.customer_id
+    WHERE invoices.product_category = 'Garden'
     """
-
+ 
     #### create product_revenue variable
-    outdoors_query = """select * from invoices 
-    join customers 
-    on invoices.customer_id = customers.customer_id
-    where invoices.product_category = 'Outdoors';
-    """
+    product_revenue =""" SELECT product_category, SUM(unit_price*quantity)
+                        FROM invoices
+                        GROUP BY product_category
+                        ORDER BY SUM DESC """
+
+
 
 ############################# TO DO ####################################
     ############ COPYING SQL QUERY OUTPUTS TO CSV FILES #############
 
     #### create master_output variable with formatted master_query
-
+    master_output = "COPY ({0}) TO STDOUT WITH CSV HEADER".format(master_query)
     #### create outdoors_output variable with formatted outdoors_query
+    outdoors_output = "COPY ({0}) TO STDOUT WITH CSV HEADER".format(outdoors_query)
 
     #### create garden_output variable with formatted garden_query
+    garden_output = "COPY ({0}) TO STDOUT WITH CSV HEADER".format(garden_query)
 
     #### create product_revenue_output variable with formatted product_revenue_query
+    product_revenue_output = "COPY ({0}) TO STDOUT WITH CSV HEADER".format(product_revenue)
 
 ############################# TO DO ####################################
 ############## CREATING CSV FILES FROM OUTPUTS #########################
 
     #### create master.csv with open(...) function ####
+    with open('master.csv','w') as file:
+        cursor.copy_expert(master_output,file)
 
     #### create outdoors.csv with open(...) function ####
+    with open('outdoors.csv','w') as file:
+        cursor.copy_expert(outdoors_output,file)
 
     #### create garden.csv with open(...) function ####
+    with open('garden.csv','w') as file:
+        cursor.copy_expert(garden_output,file)
 
     #### create product_revenue.csv with open(...) function ####
+    with open('product_revenue.csv','w') as file:
+        cursor.copy_expert(product_revenue_output,file)
 
 except (Exception, Error) as error:
     print("Error while connecting to PostgreSQL DB", error)
